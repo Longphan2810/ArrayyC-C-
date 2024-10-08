d775fb2da8956ca349a36e2c73e7b068e7f7bd4d
Long Phan <100278075+Longphan2810@users.noreply.github.com>
2024-10-08 16:56:15 +0700

    Update arrayFail.cpp

--- srcFail/arrayFail.cpp
+++ srcFail/arrayFail.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     // int a[10] = {2, 3, 19, 88, 100, 28, 47, 31, 14, 25};
-    int nt[5]= {2,3,19,47,31}
+    int nt[5]= {2,3,19,47,31};
     cout << "Cac so nguyen to trong mang la: ";
     for (int i = 0; i < 5; i++) {
             cout << nt[i] << " ";   
